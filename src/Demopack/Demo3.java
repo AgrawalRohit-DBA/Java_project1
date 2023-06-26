bb1466582abe47ec8e0aa803230f1be5fe528cd4
Agrawal Rohit <rsagrawal.13@gmail.com>
2023-06-26 17:44:35 +0530

    class3 added

---
+++ src/Demopack/Demo3.java
@@ -0,0 +1,11 @@
+package Demopack;
+
+public class Demo3 {
+	public static void main(String arg[])
+	{
+		System.out.println("This is the main method...");
+		System.out.println("This is simple example");
+	}
+
+
+}
