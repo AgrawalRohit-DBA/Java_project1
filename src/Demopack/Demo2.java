2dfe475e00cb7b7272fe7de9fc47d25ca67c74a0
Agrawal Rohit <rsagrawal.13@gmail.com>
2023-06-26 17:37:06 +0530

    New Demo2 class created

---
+++ src/Demopack/Demo2.java
@@ -0,0 +1,10 @@
+package Demopack;
+
+public class Demo2 {
+	public static void main(String arg[])
+	{
+		System.out.println("This is the main method...");
+		System.out.println("This is simple example");
+	}
+
+}
